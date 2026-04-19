2f680d9ff9dbd74b094d37d85e3bc6bc7c6e7f9d
Aditya Sharma <rajadityasharma@gmail.com>
2026-04-19 18:11:13 +0530

    update

--- src/components/GallerySection.jsx
+++ src/components/GallerySection.jsx
@@ -1,20 +1,24 @@
 import { useState } from "react";
 import "./GallerySection.css";
 
-import img1 from "../assets/Hr_power_energy_ad1.webp";
-import img2 from "../assets/Hr_power_energy_ad2.webp";
-import img3 from "../assets/Hr_power_energy_ad3.webp";
-import img4 from "../assets/Hr_power_energy_ad4.webp";
-import img5 from "../assets/service-solar.webp";
-import video1 from "../assets/HR_Power_energy_slide1.mp4";
+import img0 from "../assets/CEO_&_Founder.webp";
+import img1 from "../assets/Shri_Sarathi_Ad1.webp";
+import img2 from "../assets/Shri_Sarathi_Ad2.webp";
+import img3 from "../assets/Shri_Sarathi_Ad3.webp";
+import img4 from "../assets/Shri_Sarathi_Ad4.webp";
+import img5 from "../assets/Shri_Sarathi_Ad5.webp";
+import img6 from "../assets/service-solar.webp";
+import video1 from "../assets/Shri_Sarathi_Solar.mp4";
 
 const galleryItems = [
+  { type: "image", src: img0 },
   { type: "image", src: img1 },
   { type: "image", src: img2 },
   { type: "image", src: img3 },
   { type: "video", src: video1 },
   { type: "image", src: img4 },
   { type: "image", src: img5 },
+  { type: "image", src: img6 },
 ];
 
 const GallerySection = () => {
